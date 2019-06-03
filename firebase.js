0ad9185df53cd530286c7ef7c9ba44d8bba5d289
Amar Yassa <amaryassa@yahoo.fr>
2019-06-04 01:39:43 +0200

    first

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC0jf5WpwGrBBWvn4xa9L3xwRN_jNUbZ_o",
+    authDomain: "shamir-31c72.firebaseapp.com",
+    databaseURL: "https://shamir-31c72.firebaseio.com",
+    projectId: "shamir-31c72",
+    storageBucket: "shamir-31c72.appspot.com",
+    messagingSenderId: "518787646885",
+    appId: "1:518787646885:web:7d38e667bcc1ec24"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
