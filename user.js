4d3abb8ea6903b1c954449ac7e8540ef9e22e8a4
Amar Yassa <amaryassa@yahoo.fr>
2019-06-04 03:07:48 +0200

    second

---
+++ user.js
@@ -0,0 +1,68 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+function partage_nopartage(id, keys,share ,min_key, max_key) {
+    firebase.database().ref('user/'+id).set({
+      keys: keys,
+      share: share,
+      min_key: min_key,
+      max_key: max_key
+    });
+  }
+
+ 
+
+  function getOneUser(id) {
+      
+    var refUser = firebase.database().ref('user/'+id);
+    refUser.on('value', function(snapshot) {
+        console.log(snapshot.val());
+         utilisateur= snapshot.val();   
+    });
+    return utilisateur;
+  }
+
+
+
+
+// $(document).ready(function(){ 
+    var mon_id = getUrlParameter('id');
+
+    if(mon_id){
+        
+        $("#admin").hide();
+        $("#user").show();
+        
+        $("#mon_id").html(mon_id);
+    }else{
+        $("#user").hide();
+        $("#admin").show();
+    }
+    
+    $("#partage").on( "click", function() {
+    utilisateur =getOneUser(mon_id);
+    partage_nopartage(mon_id,utilisateur.keys,1, utilisateur.min_key ,utilisateur.max_key )
+    console.log(getOneUser(mon_id));
+    getData() ;
+    });
+    
+    $("#no_partage").on( "click", function() {
+        utilisateur =getOneUser(mon_id);
+        partage_nopartage(mon_id,utilisateur.keys,0, utilisateur.min_key ,utilisateur.max_key)
+        console.log(getOneUser(mon_id))
+        getData() ;
+        });
+
+// });
